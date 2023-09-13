b1b105f8fb55a38afa121fcf2f93c2525b1b7b46
T8840 <673683677@qq.com>
2023-09-13 15:34:20 +0800

    Site updated: 2023-09-13 15:34:18

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156/","posts/3e80a85d/","posts/d9ef328/","posts/b9a928cc/","posts/9daba997/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["posts/4a17b156/","posts/3e80a85d/","posts/d9ef328/","posts/b9a928cc/","posts/9daba997/","posts/77571803/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
